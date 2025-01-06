9bb6df02feaccc470d8c643c7e37dfc7e9bdeadf
yudtie <35577761+yudtie@users.noreply.github.com>
2025-01-05 20:06:31 -0500

    readding AWE page images

--- pages/awe.tsx
+++ pages/awe.tsx
@@ -7,31 +7,27 @@ const AWE: NextPage = () => {
    
   return (
     <div className="text-lg md:text-lg xl:text-xl ">
-      <Head>
-        <title>AWE page</title>
-      </Head>
+    <Head>
+    <title>AWE page</title>
+    </Head>
 
-      <Header />
+    <Header />
 
-      <div className="mx-auto pt-10 max-w-[1600px]">
-        <h1 classname="text-center">AWE Brand!</h1>
-        <p className="text-center">I was excited to get the contract to design and build the AWE Brand website in wordpress.</p>
-        
-        <div className="flex flex-col flex-shrink-0 md:flex-row">
-            <div className="w-2/3"><img src="/awebrand-hero.jpg" alt="" /></div>
-            <div className="w-1/3"><img src="/awebrand-1.jpg" alt="" /><img src="/awebrand-2.jpg" alt="" /></div>
-        </div>
-
-
-      </div>
-
-      
-
-
-      <Footer />
+    <div className="mx-auto pt-10 max-w-[1600px]">
+    <h1 className="text-center">AWE Brand!</h1>
+    <p className="text-center">I was excited to get the contract to design and build the AWE Brand website in wordpress.</p>
+    
+    <div className="flex flex-col flex-shrink-0 md:flex-row">
+        <div className="w-2/3"><img src="/awebrand-hero.jpg" alt="" /></div>
+        <div className="w-1/3"><img src="/awebrand-1.jpg" alt="" /><img src="/awebrand-2.jpg" alt="" /></div>
+    </div>
+    </div>
+    
+    <Footer />
 
     </div>
   );
 };
+
 
 export default AWE;
